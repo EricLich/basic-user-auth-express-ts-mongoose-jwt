65e9d8144d1e7df4307ce4412f0b2d9bde6975ac
Eric Lich <eric.lichtensztein@aleia.io>
2022-12-26 17:27:29 -0300

    add login method with encryption and jwt

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 import express, { Request, Response, NextFunction } from 'express';
 import userCtrl from './controllers/user.controller';
 import connect from './utils/dbConnect';
@@ -8,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, async (): Promise<void> => {
   try {
@@ -34,21 +36,29 @@ app.get('/users', async (req: Request, res: Response, next: NextFunction): Promi
 
 app.post('/users', emptyChecker, async (req: TypedRequestBody<{ name: string, email: string, password: string }>, res: Response, next: NextFunction): Promise<void> => {
   try {
-    const newUser = await userCtrl.createUser({ name: req.body.name, email: req.body.email, password: req.body.password });
-    res.send({ newUser }).status(200);
-  } catch (err) {
-    res.send('There was an error creating user').status(400);
+    await userCtrl.createUser({ name: req.body.name, email: req.body.email, password: req.body.password }, res);
+  } catch (err: any) {
+    res.send(err.message).status(400);
   }
 });
 
-/* app.post('/login', async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-
-}) */
+app.post('/users/login', async (req: TypedRequestBody<{ email: string, password: string }>, res: Response, next: NextFunction): Promise<void> => {
+  if (!req.body.email && !req.body.password) {
+    res.send('Email and password must be filled').status(400)
+  } else {
+    try {
+      const loginInfo = await userCtrl.login(req.body.email, req.body.password);
+      res.json({ loginInfo }).status(200);
+    } catch (error: any) {
+      res.send(error.message).status(400)
+    }
+  }
+})
 
 app.delete('/users/:id', async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     await userCtrl.deleteUser(req.params.id);
-    res.send('User deleted').status(200);
+    res.json('User deleted').status(200);
   } catch (error) {
     res.send('Error deleting user').status(500);
   }
--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,24 +1,56 @@
 import { DocumentDefinition } from 'mongoose';
 import UserModel, { UserDocument } from '../models/user.model';
-
+import jwt from 'jsonwebtoken';
+import { Response } from 'express';
+import validator from 'validator'
+import bcrypt from 'bcrypt'
 export interface UserController {
-  createUser: (user: DocumentDefinition<UserDocument>) => void,
+  createUser: (user: DocumentDefinition<UserDocument>, res: Response) => void,
   getUsers: () => Promise<UserDocument[]>,
   deleteUser: (id: string) => Promise<void>,
+  login: (email: string, password: string) => Promise<{ user: UserDocument, token: string }>
+}
+
+const createToken = (_id: string) => {
+  // @ts-ignore
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' });
 }
 
 const userCtrl: UserController = {
-  createUser: async (user: DocumentDefinition<UserDocument>): Promise<UserDocument> => {
+  createUser: async (user: DocumentDefinition<UserDocument>, res: Response): Promise<void> => {
+    if (!validator.isEmail(user.email)) throw new Error('Email is not valid');
+    if (!validator.isStrongPassword(user.password)) throw new Error('Password not strong enough');
+
+    const exists = await UserModel.findOne({ email: user.email });
+    if (exists) throw new Error('User already exists');
+
     try {
       const newUser = await UserModel.create({
         name: user.name,
-        email: user.password,
+        email: user.email,
         password: user.password
       });
-      return newUser
+      //create token
+      const token = createToken(newUser.id);
+      res.status(200).json({ email: newUser.email, token });
     } catch (error: any) {
       throw new Error(error.message);
     }
+  },
+  login: async (email: string, password: string): Promise<{ user: UserDocument, token: string }> => {
+    if (!email || !password) throw new Error('All fields must be filled');
+
+    const user = await UserModel.findOne({ email });
+
+    if (!user) throw new Error('Incorrect email');
+
+    //check password
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) throw new Error('Email or password not correct');
+    const token = createToken(user.id);
+
+    return { user, token };
   },
   getUsers: async () => {
     try {
